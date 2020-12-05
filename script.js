c60e8007fc244786fd52e09d63ba576ee573c956
saddamarbaa <>
2020-12-06 01:03:11 +0700

    Start javascript file

---
+++ script.js
@@ -0,0 +1,3 @@
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
